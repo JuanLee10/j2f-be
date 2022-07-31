6c4a74df9e62ad900c4c853141441e794906d8c5
Code Mode <codemode@Juans-MacBook-Pro.local>
2022-07-30 21:06:12 -0700

    added applications and testing

--- models/user.js
+++ models/user.js
@@ -138,6 +138,15 @@ class User {
 
     if (!user) throw new NotFoundError(`No user: ${username}`);
 
+    const applicationsRes = await db.query(
+      `SELECT a.job_id
+        FROM applications AS a
+        WHERE a.username = $1`,
+      [username]
+    );
+    user.applications = applicationsRes.rows.map(a => a.job_id);
+    
+
     return user;
   }
 
@@ -201,6 +210,34 @@ class User {
 
     if (!user) throw new NotFoundError(`No user: ${username}`);
   }
+
+  /** Apply for job: update db, returns undefined.
+   *
+   * - username: username applying for job
+   * - jobId: job id
+   **/
+  static async applyToJob(username, jobId) {
+    const preCheck = await db.query(
+          `SELECT id
+           FROM jobs
+           WHERE id = $1`, [jobId]);
+    const job = preCheck.rows[0];
+
+    if (!job) throw new NotFoundError(`No job: ${jobId}`);
+
+    const preCheck2 = await db.query(
+          `SELECT username
+           FROM users
+           WHERE username = $1`, [username]);
+    const user = preCheck2.rows[0];
+
+    if (!user) throw new NotFoundError(`No username: ${username}`);
+
+    await db.query(
+          `INSERT INTO applications (job_id, username)
+           VALUES ($1, $2)`,
+        [jobId, username]);
+  } 
 }
 
 module.exports = User;
